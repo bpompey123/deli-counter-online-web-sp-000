59591c30a5fcc6e0358032ce338688c9ac436d7a
Brian Pompey <brianthescholar@gmail.com>
2019-08-01 02:35:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -28,6 +28,6 @@ def now_serving(katz_deli)
     puts "There is nobody waiting to be served!"
   else
     puts "Currently serving #{katz_deli[0]}."
-  katz_deli.unshift
   end
+  katz_deli.unshift
 end
