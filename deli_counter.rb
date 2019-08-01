52f15b13ab9fa92f544be36e87eb246149a1f784
Brian Pompey <brianthescholar@gmail.com>
2019-08-01 01:19:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,6 +10,6 @@ def line(katz_deli)
   if other_deli.count == 0
     puts "The line is currently empty."
   else
-    puts "The line is currently " + other_deli.join(1)
+    puts "The line is currently " + other_deli.join("1")
   end
 end
