df1d992a2ef09c3327e0d5011fcaf48d91c8ff1d
Brian Pompey <brianthescholar@gmail.com>
2019-08-01 00:49:01 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,10 @@
 katz_deli = []
 
 def line(katz_deli)
-  puts "The line is currently empty."
+  if katz_deli == 0
+    puts "The line is currently empty."
+  elsif katz_deli >= 1
+    puts "p"
+  end
+    
 end
